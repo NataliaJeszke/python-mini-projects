3bd45d06f378c858413272a0ab39737f981b5a57
NataliaJeszke <nataliajeszke8@gmail.com>
2023-07-31 16:50:16 +0200

    added main function with method

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,16 +1,17 @@
-global result
+def main():
+    print("Hello! This is calculator in Python")
+    print("Give first number")
+    num1 = float(input())
+    print("Give second number")
+    num2 = float(input())
+    print("Write one of: + , - , /, *")
+    calculate = input()
 
-print("Hello! This is calculator in Python")
-print("Give first number")
-num1 = float(input())
-print("Give second number")
-num2 = float(input())
-print("Write one of: + , - , /, *")
-calculate = input()
+    calculator(num1, num2, calculate)
 
 
 def calculator(number1, number2, calculate1):
-    global result
+    result = 0
     if calculate1 == "+":
         result = number1 + number2
 
@@ -32,4 +33,5 @@ def calculator(number1, number2, calculate1):
         print(result)
 
 
-calculator(num1, num2, calculate)
+if __name__ == "__main__":
+    main()
